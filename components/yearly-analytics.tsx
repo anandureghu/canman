ae76e463971ecf1e72681f84face952fc3ed9041
Anandu Reghu <anandukr145@gmail.com>
2025-08-04 15:50:04 +0530

    feat: added pending stock info on analytics (#8)

--- components/yearly-analytics.tsx
+++ components/yearly-analytics.tsx
@@ -107,6 +107,14 @@ const YearlyAnalytics = () => {
         />
 
         <InfoCard
+          title="Pending Stock"
+          info={(data?.supplied! - data?.collected!).toString() || "0"}
+          description="" // Replace with actual delivered value
+          infoStyle="bg-yellow-50 border border-yellow-500"
+          infoTextStyle="text-orange-700"
+        />
+
+        <InfoCard
           title="Stock In-Hand"
           info={data?.stock.toString() || "0"}
           description="" // Replace with actual delivered value
